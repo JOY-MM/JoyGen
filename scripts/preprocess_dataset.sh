6930c3ece4fecd9f46fe16b3d6b715bbeea36b37
gobigrassland <793549651@qq.com>
2025-01-02 14:01:05 +0800

    feat: set up github pages

--- scripts/preprocess_dataset.sh
+++
@@ -1,8 +0,0 @@
-python -u preprocess_dataset.py \
-    --checkpoints_dir ./pretrained_models \
-    --name face_recon_feat0.2_augment \
-    --epoch=20 \
-    --use_opengl False \
-    --bfm_folder ./pretrained_models/BFM \
-    --video_dir ./demo \
-    --result_dir ./results/preprocessed_dataset \
--- scripts/train_joygen.sh
+++
@@ -1,2 +0,0 @@
-accelerate launch --main_process_port 29501 --config_file config/accelerate_config.yaml train_joygen.py
-
